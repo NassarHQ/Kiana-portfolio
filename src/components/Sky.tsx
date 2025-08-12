d9f86617895cc487c5307ab7d79dfd1cb5bae99b
Mo <mohammed.nassar@sjsu.edu>
2025-08-11 22:30:19 -0700

    Initialized Repo

---
+++ src/components/Sky.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import React from "react";
+import Moon from "@/components/Moon";
+
+export default function Sky() {
+  return (
+    <div className="sky bg-gradient-to-t from-blue-800 via-blue-950 to-slate-950 h-screen w-full flex items-center justify-center">
+      <div className="rounded-full absolute top-[10%] right-[12%] drop-shadow-[0_0_20px_rgba(0,255,255,075)]">
+        <Moon />
+      </div>
+    </div>
+  );
+}
