392af8fc6b8673be455c8362f0ef601ed29ec5bd
Mo <mohammed.nassar@sjsu.edu>
2025-08-11 22:33:36 -0700

    Implemented Animation for the moon

--- src/components/Sky.tsx
+++ src/components/Sky.tsx
@@ -5,7 +5,7 @@ import Moon from "@/components/Moon";
 export default function Sky() {
   return (
     <div className="sky bg-gradient-to-t from-blue-800 via-blue-950 to-slate-950 h-screen w-full flex items-center justify-center">
-      <div className="rounded-full absolute top-[10%] right-[12%] drop-shadow-[0_0_20px_rgba(0,255,255,075)]">
+      <div className="top-[12%] motion-safe:animate-[float_4s_ease-in-out_infinite] rounded-full absolute top-[10%] right-[12%] drop-shadow-[0_0_20px_rgba(0,255,255,075)]">
         <Moon />
       </div>
     </div>
